d90cf85a02adeff331cfebb3f3cc44de53d7ab0c
nnickd <nicolas.dellitalia@gmail.com>
2017-01-17 00:14:02 -0500

    toggle field

--- app/curl/curl.controller.js
+++ app/curl/curl.controller.js
@@ -5,9 +5,15 @@ angular
 function CurlController($scope, $sce) {
   $scope.curl = '';
   $scope.newCurl = $sce.trustAsResourceUrl($scope.curl);
+  $scope.hide = false;
 
   $scope.updateCurl = function() {
     $scope.newCurl = $sce.trustAsResourceUrl($scope.curl);
+  }
+
+  $scope.hideField = function() {
+    console.log($scope.hide)
+    $scope.hide = !$scope.hide;
   }
 
 }
